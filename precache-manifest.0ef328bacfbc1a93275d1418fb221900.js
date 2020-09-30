5c0e70fb543a8d41f87bbe13e19c7c40364cdb68
Karina <kary.vertti@gmail.com>
2020-09-30 12:46:05 -0400

    Updates

---
+++ precache-manifest.0ef328bacfbc1a93275d1418fb221900.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c2aea4f9f00b0163767b0a97784864b",
+    "url": "/karina-vertti-project-five/index.html"
+  },
+  {
+    "revision": "bd32e58606b2beb79b5b",
+    "url": "/karina-vertti-project-five/static/css/main.bc34c030.chunk.css"
+  },
+  {
+    "revision": "89666ff44610aff7a030",
+    "url": "/karina-vertti-project-five/static/js/2.1a6f1a69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/karina-vertti-project-five/static/js/2.1a6f1a69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd32e58606b2beb79b5b",
+    "url": "/karina-vertti-project-five/static/js/main.a806f4cd.chunk.js"
+  },
+  {
+    "revision": "7aed9a939df7b9cdf216",
+    "url": "/karina-vertti-project-five/static/js/runtime-main.8f314233.js"
+  }
+]);
